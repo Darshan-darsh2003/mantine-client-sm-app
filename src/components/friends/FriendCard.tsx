d1098696db96d7d54cf808001bb6c88001cc02fa
Darshan-darsh2003 <poojaridarsu9550@gmail.com>
2024-01-09 19:11:31 +0530

    Add FriendCard component and update SearchPage

--- src/components/friends/FriendCard.tsx
+++ src/components/friends/FriendCard.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import classes from "./FriendCard.module.css";
+import { Avatar, Text, Group, Box, Paper, ActionIcon, Tooltip } from "@mantine/core";
+import { IconPhoneCall, IconAt, IconUserPlus } from "@tabler/icons-react";
 
-const FriendCard = () => {
+export function FriendCard() {
   return (
-    <div>FriendCard</div>
-  )
-}
+    <Box>
+      <Group>
+        <Paper
+          withBorder
+          py={10}
+          px={20}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            // gap: 20,
+          }}
+        >
+          <Avatar
+            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png"
+            size={100}
+            radius="md"
+          />
+          <Box px={20}>
+            <Text fz="xs" tt="uppercase" fw={700} c="dimmed">
+              Software engineer
+            </Text>
 
-export default FriendCard
+            <Text fz="lg" style={{
+              whiteSpace: "nowrap",
+            }} fw={500} className={classes.name}>
+              Robert Glassbreaker
+            </Text>
+
+            <Group wrap="nowrap" gap={10} mt={3}>
+              <IconAt stroke={1.5} size="1rem" className={classes.icon} />
+              <Text fz="xs" c="dimmed">
+                robert@glassbreaker.io
+              </Text>
+            </Group>
+
+            <Group wrap="nowrap" gap={10} mt={5}>
+              <IconPhoneCall
+                stroke={1.5}
+                size="1rem"
+                className={classes.icon}
+              />
+              <Text fz="xs" c="dimmed">
+                +11 (876) 890 56 23
+              </Text>
+            </Group>
+          </Box>
+          <Box px={20}>
+           <Tooltip label="Add Friend">
+           <ActionIcon
+              variant="default"
+              // color="blue"
+              radius="md"
+              size="xl"
+              style={{ width: "auto", height: "auto" }}
+            >
+              <IconUserPlus stroke={1.5} size="1rem" />
+            </ActionIcon>
+           </Tooltip>
+          </Box>
+        </Paper>
+      </Group>
+    </Box>
+  );
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
 import {
   Autocomplete,
-  Box,
   Button,
   Grid,
   Group,
@@ -11,6 +10,7 @@ import {
 } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
 import Stories from "../../components/stories/Stories";
+import { FriendCard } from "../../components/friends/FriendCard";
 // maxHeight: "calc(100vh - 200px)",
 
 const Home = () => {
@@ -82,18 +82,20 @@ const Home = () => {
         </Grid.Col>
       </Grid.Col>
       <Grid.Col span={{ xl: 3.5, lg: 3.5, md: 12, sm: 12, xs: 12 }}>
-        <Group>
-          <Box
-            p={5}
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
-            }}
-          >
+        <Grid
+          p={5}
+          // style={{
+          //   display: "flex",
+          //   justifyContent: "space-between",
+          //   width: "100%",
+          // }}
+        >
+          <Grid.Col span={6}>
             <Title w="100%" display="flex" order={3}>
               Your Friends
             </Title>
+          </Grid.Col>
+          <Grid.Col span={6}>
             <Autocomplete
               placeholder="Search Friend"
               leftSection={
@@ -105,83 +107,21 @@ const Home = () => {
               data={["John Doe", "Neil Azmuth", "Jackie Chan", "Wielder Smith"]}
               visibleFrom="xs"
             />
-          </Box>
-          <ScrollArea h={{xl:'75vh',lg:'75vh'}} w="100%" style={{ border: "1px solid #1BAAF7" }}>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-          </ScrollArea>
-        </Group>
-        {/* <Group mt={25}>
-          <Box
-            p={5}
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
-            }}
-          >
-            <Title w="100%" display="flex" order={3}>
-              Friend Suggestions
-            </Title>
-            <Autocomplete
-              placeholder="Search People"
-              leftSection={
-                <IconSearch
-                  style={{ width: rem(16), height: rem(16) }}
-                  stroke={1.5}
-                />
-              }
-              data={["John Doe", "Neil Azmuth", "Jackie Chan", "Wielder Smith"]}
-              visibleFrom="xs"
-            />
-          </Box>
-          <ScrollArea
-            h={{ xl: "35vh", lg: "35vh", md: 400, sm: 400, xs: 400 }}
-            w="100%"
-            style={{ border: "1px solid #1BAAF7" }}
-          >
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-          </ScrollArea>
-        </Group> */}
+          </Grid.Col>
+        </Grid>
+        <ScrollArea
+          h={{ xl: "90vh", lg: "90vh" }}
+          // w="100%"
+          style={{ border: "1px solid #1BAAF7" }}
+        >
+          {Array(10)
+            .fill(null)
+            .map((element, index) => (
+              <Grid.Col span={{ lg: 4, xl: 4, md: 6, sm: 6, xs: 12 }}>
+                <FriendCard />
+              </Grid.Col>
+            ))}
+        </ScrollArea>
       </Grid.Col>
     </Grid>
   );
